9b4039f977340b8963c285524d8333230672a152
Bora Semiz <borasemiz50@gmail.com>
2024-03-08 11:20:33 +0100

    fix completions on

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,112 +1,112 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      local formatters = {
-        "stylua",
-        "prettier",
-        "prettierd",
-      }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			local formatters = {
+				"stylua",
+				"prettier",
+				"prettierd",
+			}
 
-      require("mason").setup()
-      local mason_registry = require("mason-registry")
+			require("mason").setup()
+			local mason_registry = require("mason-registry")
 
-      mason_registry:on(
-        "package:handle",
-        vim.schedule_wrap(function(pkg, _)
-          print(string.format("Installing %s", pkg.name))
-        end)
-      )
+			mason_registry:on(
+				"package:handle",
+				vim.schedule_wrap(function(pkg, _)
+					print(string.format("Installing %s", pkg.name))
+				end)
+			)
 
-      mason_registry:on(
-        "package:install:success",
-        vim.schedule_wrap(function(pkg, _)
-          print(string.format("Successfully installed %s", pkg.name))
-        end)
-      )
+			mason_registry:on(
+				"package:install:success",
+				vim.schedule_wrap(function(pkg, _)
+					print(string.format("Successfully installed %s", pkg.name))
+				end)
+			)
 
-      for _, formatter in ipairs(formatters) do
-        if not mason_registry.is_installed(formatter) then
-          local package = mason_registry.get_package(formatter)
-          package:install()
-        end
-      end
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "jsonls",
-          "cssls",
-          "html",
-          "gopls",
-          "eslint",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			for _, formatter in ipairs(formatters) do
+				if not mason_registry.is_installed(formatter) then
+					local package = mason_registry.get_package(formatter)
+					package:install()
+				end
+			end
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"jsonls",
+					"cssls",
+					"html",
+					"gopls",
+					"eslint",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = {
-                "vim",
-              },
-            },
-          },
-        },
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				--capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = {
+								"vim",
+							},
+						},
+					},
+				},
+			})
+			lspconfig.html.setup({
+				--capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
 
-      -- Global mappings.
-      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+			-- Global mappings.
+			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-          -- Buffer local mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-          vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-          vim.keymap.set("n", "<leader>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, opts)
-          vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
-        end,
-      })
-    end,
-  },
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wl", function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
+				end,
+			})
+		end,
+	},
 }
